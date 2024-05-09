f2d770f108ecc0b153f037767bac558b075b41a7
Gaurav Panchal <gkp.125@gmail.com>
2024-05-09 15:56:06 +0530

    Prettier Added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express from "express";
+import cors from 'cors'
+import express from 'express'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get("/", (req, res) => {
-  return res.send("HELLO WORLD");
-});
+app.get('/', (_, res) => {
+  return res.send('HELLO WORLD')
+})
 
 app.listen(3001, () => {
-  console.log("Listening on Port ", 3001);
-});
+  console.log('Listening on Port ', 3001)
+})
