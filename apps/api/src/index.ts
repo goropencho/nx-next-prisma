0a198a38356d13bd1e2715c0141a1cb736f04811
Gaurav Panchal <gkp.125@gmail.com>
2024-05-09 15:52:08 +0530

    NX initialized

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD");
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.send("HELLO WORLD");
+});
+
+app.listen(3001, () => {
+  console.log("Listening on Port ", 3001);
+});
